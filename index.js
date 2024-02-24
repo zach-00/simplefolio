6f803f6cab9e935248d9a6686fd4175d7463c19f
Zach <walkowiakzach05@gmail.com>
2024-02-23 17:28:29 -0800

    Moved styling files to ensure styling is present in deployed pages site

---
+++ index.js
@@ -0,0 +1,6 @@
+import initScrollReveal from "./src/scripts/scrollReveal";
+import initTiltEffect from "./src/scripts/tiltAnimation";
+import { targetElements, defaultProps } from "./src/data/scrollRevealConfig";
+
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
